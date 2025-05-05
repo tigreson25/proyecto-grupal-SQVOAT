99d431efc84271adfb978eb1a9478d01394fd471
Santiago <sakyqv0806@gmail.com>
2025-05-05 19:48:40 +0200

    Merge final

---
+++ main.py
@@ -0,0 +1,12 @@
+import script
+import gui
+
+def main():
+    # Llamar a la funcionX de script.py (Alumno B)
+    script.funcionX()
+
+    # Llamar a la mostrarGUI de gui.py (Alumno C)
+    gui.mostrarGUI()
+
+if __name__ == "__main__":
+    main()
